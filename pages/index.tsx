d99e2c39f868263e4cf622c8fd53d77e78262bf5
yungblud <yungblud@kakao.com>
2021-08-10 13:19:05 +0900

    feat: modified index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,37 @@
 import { NextPage } from 'next';
 import React from 'react';
+import { Typography, Button } from 'antd';
+import { useAuth } from '@/components/auth/hooks/auth_hooks';
 
-const IndexPage: NextPage = () => <>2021 cochl live</>;
+const IndexPage: NextPage = () => {
+  const { haveUser } = useAuth();
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 'calc(100vh)',
+      }}
+    >
+      <Typography>
+        <Typography.Title style={{ textAlign: 'center' }}>Welcome To Cochl Live</Typography.Title>
+        <Typography.Paragraph style={{ textAlign: 'center', fontSize: 19 }}>
+          Are you ready to join the live quiz?
+        </Typography.Paragraph>
+      </Typography>
+      {haveUser ? (
+        <Button type="primary" href="/quiz/dongho/join">
+          Let's Join!
+        </Button>
+      ) : (
+        <Button type="primary" href="/signin?redirect=/quiz/dongho/join">
+          Login via google!
+        </Button>
+      )}
+    </div>
+  );
+};
 
 export default IndexPage;
